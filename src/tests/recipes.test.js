454dc8d5fc0ebb2e96c20c1a20a4ac412837f56a
Gustavo Cardoso <gustavocardoso@gmail.com>
2019-07-31 17:15:02 -0700

    Remove folder

--- src/tests/recipes.test.js
+++
@@ -1,7 +0,0 @@
-import { expect } from 'chai'
-import Recipes from '../js/recipes'
-
-test('should return a list of recipes', async () => {
-  const recipes = await Recipes.getRecipes()
-  expect(true).to.be.equal(true)
-})
